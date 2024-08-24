588fb4db93b80df21c4b8b75e06e99f7199ef4a1
danny francisco <dcfran@gmail.com>
2024-08-25 00:38:48 +0800

    first commit

---
+++ lib/balloon_crazy.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:balloon_crazy/components/play_area.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+import 'components/components.dart';
+import 'config.dart';
+
+class BalloonCrazy extends FlameGame with HasCollisionDetection {
+  BalloonCrazy()
+      : super(
+          camera: CameraComponent.withFixedResolution(
+              width: gameWidth, height: gameHeight),
+        );
+
+  final rand = math.Random();
+  double get width => size.x;
+  double get height => size.y;
+
+  @override
+  FutureOr<void> onLoad() async {
+    super.onLoad();
+
+    camera.viewfinder.anchor = Anchor.topLeft;
+
+    world.add(PlayArea());
+  }
+}
