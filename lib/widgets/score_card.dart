6ca619f60e37eba329ef1dcad0984d5f1863bc32
danny francisco <dcfran@gmail.com>
2024-09-18 05:40:26 +0800

    Adjust font sizes in game_title.dart and score_card.dart

--- lib/widgets/score_card.dart
+++ lib/widgets/score_card.dart
@@ -17,7 +17,9 @@ class ScoreCard extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(12, 6, 12, 18),
           child: Text(
             'Score: $score'.toUpperCase(),
-            style: Theme.of(context).textTheme.titleLarge!,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontSize: 15.0,
+                ),
           ),
         );
       },
