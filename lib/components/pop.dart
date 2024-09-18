f303b438141a37ced668b847d1a6467eded85934
danny francisco <dcfran@gmail.com>
2024-09-18 21:42:07 +0800

    Refactor pop component to use sprite sheet animation

---
+++ lib/components/pop.dart
@@ -0,0 +1,34 @@
+import 'package:flame/cache.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+
+class Pop extends SpriteAnimationComponent {
+  Pop({
+    required Vector2 position,
+    required Vector2 size,
+  }) : super(
+          position: position,
+          size: size,
+          anchor: Anchor.center,
+        );
+
+  late final Images images;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    images = Images();
+    final spriteSheet = await images.load('pop_sprite_sheet.png');
+    final spriteSize = Vector2(32, 32); // Assuming each frame is 32x32 pixels
+    final spriteSheetData = SpriteSheet(
+      image: spriteSheet,
+      srcSize: spriteSize,
+    );
+
+    animation = spriteSheetData.createAnimation(
+      row: 0,
+      stepTime: 0.1,
+      to: 3,
+    );
+  }
+}
